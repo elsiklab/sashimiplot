54b6cec36dcaacd18591b2faf5e22f853ae1e988
Colin <colin.diesh@gmail.com>
2016-04-14 01:33:08 -0500

    Initial test case

---
+++ test/spec/Sashimi.spec.js
@@ -0,0 +1,20 @@
+require([
+            'dojo/_base/declare',
+            'dojo/_base/array',
+            'JBrowse/Browser',
+        ], function( declare, array, Browser ) {
+
+
+
+describe( '1==1', function() {
+              
+
+
+              it('math', function() {
+                  expect(1==1).toBeTruthy();
+              });
+});
+
+
+});
+
